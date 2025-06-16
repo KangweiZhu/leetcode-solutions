a268e959325f131fab299c267906d1205b38b308
AnicaaOvO <anicaazhu@gmail.com>
2025-06-15 19:04:54 -0500

    Time: 0 ms (100.00%) | Memory: 17.7 MB (57.69%) - LeetSync

--- 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
+++ 1529-max-difference-you-can-get-from-changing-an-integer/max-difference-you-can-get-from-changing-an-integer.py
@@ -1,33 +1,30 @@
 class Solution:
     def maxDiff(self, num: int) -> int:
+        left = 0
         stringnified_num = str(num)
         m = len(stringnified_num)
         right = m - 1
-        
-        # 构造最大值
-        ptr = 0
         max_num = num
+        ptr = 0
         while ptr <= right:
             if stringnified_num[ptr] != '9':
-                max_version = stringnified_num.replace(stringnified_num[ptr], '9')
-                max_num = int(max_version)
+                stringnified_num = stringnified_num.replace(stringnified_num[ptr], '9')
+                max_num = max(max_num, int(stringnified_num))
                 break
             ptr += 1
-
-        # 构造最小值
-        ptr = 0
+        
+        ptr = left
         min_num = num
         stringnified_num = str(num)
         while ptr <= right:
-            ch = stringnified_num[ptr]
-            if ch != '0' and ch != '1':
+            if stringnified_num[ptr] != '0' and stringnified_num[ptr] != '1':
                 if ptr == 0:
-                    # 首位不能替换为 0，只能替换为 1
-                    min_version = stringnified_num.replace(ch, '1')
+                    stringnified_num = stringnified_num.replace(stringnified_num[ptr], '1')
                 else:
-                    min_version = stringnified_num.replace(ch, '0')
-                min_num = int(min_version)
+                    stringnified_num = stringnified_num.replace(stringnified_num[ptr], '0')
+                min_num = min(min_num, int(stringnified_num))
                 break
             ptr += 1
-
         return max_num - min_num
+        
+        

f8a7282a7563d93f3d03c906969bd84abd078e51
AnicaaOvO <anicaazhu@gmail.com>
2025-06-28 13:59:58 -0500

    Time: 39 ms (20.71%) | Memory: 18.3 MB (35.98%) - LeetSync

--- 51-n-queens/n-queens.py
+++ 51-n-queens/n-queens.py
@@ -1,61 +1,50 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        '''
-            q q q .
-            q x q q
-            q q q .
-            . q . q
-
-        '''
-        chessboard = [['.'] * n for _ in range(n)]
-
-        def isValid(curr_row, curr_col) -> bool:
-            if curr_row < 0 or curr_col < 0 or curr_row >= n or curr_col >= n:
-                return False
-            
-            # top left
-            row_start = curr_row - 1
-            col_start = curr_col - 1
-            while row_start >= 0 and col_start >= 0:
-                if chessboard[row_start][col_start] == 'Q':
-                    return False
-                row_start -= 1
-                col_start -= 1
-
-            # top right
-            row_start = curr_row - 1
-            col_start = curr_col + 1
-            while row_start >= 0 and col_start < n:
-                if chessboard[row_start][col_start] == 'Q':
-                    return False
-                row_start -= 1
-                col_start += 1
-
-            # vertical
-            for i in range(n):
-                if i != curr_row:
-                    if chessboard[i][curr_col] == 'Q':
-                        return False
-
-            # horizontal
-            for i in range(n):
-                if i != curr_col:
-                    if chessboard[curr_row][i] == 'Q':
-                        return False
-            
-            return True
-        
         result = []
+        result_builder = [['.' for _ in range(n)] for _ in range(n)]
 
-        def backtrack(start_row):
-            if start_row == n:
-                result.append([''.join(row) for row in chessboard])
+        def isValid(row, col):
+            # top left
+            tmp_row = row - 1
+            tmp_col = col - 1
+            while tmp_row >= 0 and tmp_col >= 0:
+                if result_builder[tmp_row][tmp_col] == 'Q':
+                    return False
+                tmp_row -= 1
+                tmp_col -= 1
+            
+            # top right 
+            tmp_row = row - 1
+            tmp_col = col + 1
+            
+            while tmp_row >= 0 and tmp_col < n:
+                if result_builder[tmp_row][tmp_col] == 'Q':
+                    return False
+                tmp_row -= 1
+                tmp_col += 1
+            
+            # vertically above
+            tmp_row = row -1
+            tmp_col = col
+            while tmp_row >= 0:
+                if result_builder[tmp_row][tmp_col] == 'Q':
+                    return False
+                tmp_row -= 1
+
+            return True
+            
+        def backtrack(row_num):
+            if row_num == n:
+                result.append([''.join(row_elements) for row_elements in result_builder])
+                return 
             for i in range(n):
-                if isValid(start_row, i):
-                    chessboard[start_row][i] = 'Q'
-                    backtrack(start_row + 1)
-                    chessboard[start_row][i] = '.'
-        
+                if isValid(row_num, i):
+                    result_builder[row_num][i] = 'Q'
+                    backtrack(row_num + 1)
+                    result_builder[row_num][i] = '.'
+
         backtrack(0)
         return result
-                
+
+        
+    
